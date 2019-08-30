7d7aeb12612e8b7eda71d1cca5aa75d8b880fe11
Hazel <hwat583@aucklanduni.ac.nz>
2019-08-30 13:45:16 +1200

    Test written and factorial function written (untested)

--- 280lab5_1.py
+++ 280lab5_1.py
@@ -1,1 +1,5 @@
-print("oh hi there!")
+def factorial(num):
+    total = 0
+    for i in range(num):
+        total += i +1
+    return total
--- test_factorial.py
+++ test_factorial.py
@@ -1,1 +1,10 @@
 import unittest
+
+from "280lab5_1" import factorial
+
+class FactorialTest(unittest.TestCase):
+    def test_factorial(self):
+        num = 5
+        result = factorial(num)
+        
+        self.assertEqual(result, 120)
